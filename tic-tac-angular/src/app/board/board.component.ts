af33a53a372850b38ed556fc3077c629e8f4c972
Keith Millar <keithmillar@hotmail.ca>
2020-08-21 13:42:51 -0400

    installed nebular theme, board working

--- tic-tac-angular/src/app/board/board.component.ts
+++ tic-tac-angular/src/app/board/board.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  squares: any[];
+  squares: string[];
   xIsNext: boolean;
   winner: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.newGame();
